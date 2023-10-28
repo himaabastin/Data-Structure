b9483df707e921b0ef0aca62e0ae3b02921fc4a7
himaabastin <111218465+himaabastin@users.noreply.github.com>
2023-10-28 14:45:15 +0530

    add Upto n numbers

---
+++ AddUpToNumbers/addUptoN.js
@@ -0,0 +1,15 @@
+//1st Method
+const addUptoN = (num) => {
+  let total = 0;
+  for (i = 0; i <= num; i++) {
+    total += i;
+  }
+  return total;
+};
+
+const startTime = new Date();
+console.log(addUptoN(1000000000));
+const endTime = new Date();
+
+const timeDiff = (endTime - startTime) / 1000;
+console.log(`Time diff = ${timeDiff} s`); //almost 1.2s
---
+++ AddUpToNumbers/addUptoN2.js
@@ -0,0 +1,14 @@
+// 2nd Method
+
+const addUpToN2=(n)=>{
+return n * (n + 1) /2
+}
+
+const startTime=Date.now()
+
+console.log(addUpToN2(1000000000));
+const endTime=Date.now()
+
+const TimeDiff=(endTime-startTime) / 1000
+
+console.log(`Time diff = ${TimeDiff} s`);  // almost 0.005s
