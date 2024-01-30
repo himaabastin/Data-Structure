a0823b34c91fe8ea26f910d04683f4b19e8c7341
himaabastin <111218465+himaabastin@users.noreply.github.com>
2024-01-30 14:24:00 +0530

    naive soln for same function

---
+++ arrays/same/naiveSolution.js
@@ -0,0 +1,25 @@
+// Write a function called "same" which accpept 2 arrays.The function should return true if every value of the array has its corresponding value squared in the second array.The frequency must be same
+//  A NAIVE SOLUTION O(N^2)
+const same = (arr1, arr2) => {
+  // if length of arrays not correct return false
+  console.log(arr1, arr2);
+  if (arr1.length !== arr2.length) {
+    return console.log(false);
+  }
+
+  for (i = 0; i < arr1.length; i++) {
+    // loop over
+    const correctIndex = arr2.indexOf(arr1[i] * arr1[i]);
+    // console.log(arr2.indexOf(i * i));
+    // console.log("correct Index", correctIndex);
+    if (correctIndex === -1) {
+      return console.log(false);
+    }
+    arr2.splice(correctIndex, 1);
+    // console.log("arr2", arr2);
+  }
+
+  return console.log(true);
+};
+
+same([1, 2, 3, 1], [9, 4, 1, 1]);
